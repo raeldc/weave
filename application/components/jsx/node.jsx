c033a629e97cf0acb825cdec470fa7f41f4793b8
Israel D. Canasa <raeldc@gmail.com>
2015-02-27 16:21:49 +0800

    Render components based on the node configuration
    The node configuration is just a json object.

--- application/components/jsx/node.jsx
+++ application/components/jsx/node.jsx
@@ -1,13 +1,77 @@
+var nodes = [
+    {
+        id: '1',
+        element: 'div',
+        className: 'row',
+        style: {
+            margin: '10px',
+            border: '1px solid green',
+        },
+        children: ['2', '3', '4']
+    },
+    {
+        id     : '2',
+        element: 'div',
+        text   : 'Col 1',
+        className: 'col-md-4',
+        style  : {
+        },
+    },
+    {
+        id     : '3',
+        element: 'div',
+        text   : 'Col 2',
+        className: 'col-md-4',
+        style  : {
+        },
+    },
+    {
+        id     : '4',
+        element: 'div',
+        text   : 'Col 4',
+        className: 'col-md-4',
+        style  : {
+        },
+    }
+];
+
 var aNode = React.createClass({
     getInitialState: function(){
-        return {};
+        return nodes[0];
     },
-    render: function() {
-        var element  = this.state.element  || 'div';
-        var children = this.state.children || this.state.text || '';
 
-        var node = React.createElement(element, [], children);
+    render: function() {
+        return this.createNode(this.state);
+    },
+
+    createNode: function(properties) {
+        var element  = properties.element  || 'div';
+        var children = properties.children || properties.text || undefined;
+
+        if(_.isArray(children)){
+            children = this.createChildNodes(properties.children);
+        }
+
+        var node = React.createElement(element, {
+            key      : properties.id,
+            style    : properties.style,
+            className: properties.className
+        }, children);
 
         return node;
+    },
+
+    createChildNodes: function(children) {
+        var self       = this;
+        var childNodes = [];
+
+        _.each(children, function(id){
+            var child = _.findIndex(nodes, {id: id});
+            if(_.isObject(nodes[child])){
+                childNodes.push(self.createNode(nodes[child]));
+            }
+        });
+
+        return childNodes;
     }
 });
