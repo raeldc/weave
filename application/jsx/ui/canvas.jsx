23def89975fba64022c31d376d9f88759aba9a8d
Israel D. Canasa <raeldc@gmail.com>
2015-03-19 21:23:17 +0800

    The Canvas is now inside an iFrame. Media Queries!
    Now we can preview CSS media queries. This is very useful when editing for different device sizes.

--- application/jsx/ui/canvas.jsx
+++ application/jsx/ui/canvas.jsx
@@ -3,22 +3,25 @@ var Nodes    = require('application/stores/nodes.js');
 var DOM      = require('application/stores/dom.js');
 var UIConfig = require('application/stores/uiconfig.js');
 var CONST    = require('application/constants/all.js');
+var Frame    = require('application/ui/frame.js');
 
 module.exports = React.createClass({
+    displayName: 'Canvas',
+
     getInitialState: function(){
         return UIConfig.getConfig();
     },
 
     render: function(){
         return (
-            <div id="alchemy-body">
+            <div id="alchemy-body" className={"frame-wrapper " + this.state.screenSize}>
+                <Frame 
+                    head={<link type='text/css' rel='stylesheet' href='css/style.css' />}
+                >
                 {Factory.createNode('root')}
+                </Frame>
             </div>
         );
-    },
-
-    configChanged: function(){
-        this.setState({quick_edit_on:UIConfig.getConfig('quick_edit_on')});
     },
 
     componentDidMount: function() {
@@ -27,5 +30,9 @@ module.exports = React.createClass({
 
     componentWillUnmount: function() {
         UIConfig.removeListener(CONST.UI_TOGGLE_QUICK_EDIT_MODE, this.configChanged);
+    },
+
+    configChanged: function(){
+        this.setState({quick_edit_on:UIConfig.getConfig('quick_edit_on')});
     }
 });
