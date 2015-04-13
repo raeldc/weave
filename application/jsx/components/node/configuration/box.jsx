af143f0c5b94fc5476ef734f542fae8d5e3393f3
Israel D. Canasa <raeldc@gmail.com>
2015-04-13 17:29:33 +0800

    CSSPropertyEditor is now PropertyInput
    And it now resides into a separate file.

--- application/jsx/components/node/configuration/box.jsx
+++ application/jsx/components/node/configuration/box.jsx
@@ -1,45 +1,5 @@
-var Nodes           = require('application/stores/nodes.js'),
-    Canvas          = require('application/stores/canvas.js'),
-    UINodeActions   = require('application/actions/node.js'),
-    UICanvasActions = require('application/actions/canvas.js');
-
-var CssPropertyEditor = React.createClass({
-    render: function() {
-        var value = this.getCurrentValue();
-        return <span className={this.props.className} contentEditable="true" dangerouslySetInnerHTML={{__html:value}} onBlur={this.saveInput} onKeyDown={this.detectEnter} />
-    },
-
-    getCurrentValue: function() {
-        return Nodes.getStore(this.props.node).get('css')[this.props.device][this.props.propertyName] || this.props.default;
-    },
-
-    saveInput: function(event) {
-        var value = event.target.innerHTML;
-
-        switch(this.props.filter.constructor) {
-            case Array:
-                if(this.props.filter.indexOf(value) === -1) {
-                    value = this.getCurrentValue();
-                }
-            break;
-            case RegExp:
-                value = String(value).match(this.props.filter).join('');
-            break;
-        }
-
-        UINodeActions.updateNodeCSS(this.props.node, this.props.device, this.props.propertyName, value);
-        UICanvasActions.nodeTouched();
-
-        event.preventDefault();
-    },
-
-    detectEnter: function(event) {
-        if(event.keyCode === 13) {
-            this.saveInput(event);
-            event.preventDefault();
-        }
-    }
-});
+var Nodes         = require('application/stores/nodes.js'),
+    PropertyInput = require('application/components/node/configuration/inputs/property.js');
 
 module.exports = React.createClass({
     render: function() {
@@ -47,7 +7,7 @@ module.exports = React.createClass({
             <div className="form-inline">
                 <h5>Box</h5>
                 <div>
-                    <CssPropertyEditor 
+                    <PropertyInput 
                         key="css-property-marginTop" 
                         node={this.props.node} 
                         default="0" 
@@ -55,7 +15,7 @@ module.exports = React.createClass({
                         filter={/\d+/i} 
                         propertyName="marginTop" 
                     />
-                    <CssPropertyEditor 
+                    <PropertyInput 
                         key="css-property-marginTopUnit" 
                         node={this.props.node} 
                         default="px" 
@@ -69,14 +29,14 @@ module.exports = React.createClass({
     },
 
     componentDidMount: function() {
-        this.stopListeningToCSSChanges = Nodes.getStore(this.props.node).getStore('css').listen(this.renderChanges);
+        this.stopListeningToCSSChanges = Nodes.getStore(this.props.node).getStore('css').listen(this.update);
     },
 
     componentWillUnmount: function() {
         this.stopListeningToCSSChanges();
     },
 
-    renderChanges: function() {
+    update: function() {
         this.forceUpdate();
     }
 });
---
+++ application/jsx/components/node/configuration/inputs/property.jsx
@@ -0,0 +1,53 @@
+var Nodes           = require('application/stores/nodes.js'),
+    UINodeActions   = require('application/actions/node.js'),
+    UICanvasActions = require('application/actions/canvas.js');
+
+module.exports = React.createClass({
+    render: function() {
+        var value = this.getCurrentValue();
+        return (
+            <span 
+                className={this.props.className} 
+                contentEditable="true" 
+                dangerouslySetInnerHTML={{__html:value}} 
+                onBlur={this.saveInput} 
+                onKeyDown={this.detectEnter} 
+            />
+        );
+    },
+
+    componentDidUpdate: function() {
+        React.findDOMNode(this).innerHTML = this.getCurrentValue();
+    },
+
+    getCurrentValue: function() {
+        return Nodes.getStore(this.props.node).get('css')[this.props.device][this.props.propertyName] || this.props.default;
+    },
+
+    saveInput: function(event) {
+        var value = event.target.innerHTML;
+
+        switch(this.props.filter.constructor) {
+            case Array:
+                if(this.props.filter.indexOf(value) === -1) {
+                    value = this.getCurrentValue();
+                }
+            break;
+            case RegExp:
+                value = String(value).match(this.props.filter).join('');
+            break;
+        }
+
+        UINodeActions.updateNodeCSS(this.props.node, this.props.device, this.props.propertyName, value);
+        UICanvasActions.nodeTouched();
+
+        event.preventDefault();
+    },
+
+    detectEnter: function(event) {
+        if(event.keyCode === 13) {
+            this.saveInput(event);
+            event.preventDefault();
+        }
+    }
+});
