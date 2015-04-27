e54424f3174992495c12c77f707b3f818d39c253
Israel D. Canasa <raeldc@gmail.com>
2015-04-27 18:11:17 +0800

    [LV] Row: Controls for Row Component
    Select the Columns Limit of a Row.
    
    Disable the Options that are lower than the total spans of its children columns.
    
    Put a check on the currntly selected value.

--- code/core/components/row/layout.js
+++ code/core/components/row/layout.js
@@ -1,31 +1,115 @@
-var LifeCycleMixin = require('core/components/node/mixins/layout/lifecycle.js'),
+var Nodes          = require('core/stores/nodes.js'),
+    LifeCycleMixin = require('core/components/node/mixins/layout/lifecycle.js'),
     EventsMixin    = require('core/components/node/mixins/layout/events.js'),
     EditModeMixin  = require('core/components/node/mixins/layout/editmode.js'),
     UIEventsMixin  = require('core/components/node/mixins/layout/uievents.js');
 
-module.exports = React.createClass({
-    mixins: [LifeCycleMixin],
+var ColumnOptions = React.createClass({
+    render: function() {
+        return (
+            <ul className="dropdown-menu">
+                <li><a href="#">1</a></li>
+            </ul>
+        )
+    }
+});
+
+var ColumnSelect = React.createClass({
+    getInitialState: function() {
+        return {open: false};
+    },
 
     render: function() {
-        if(_.isEmpty(this.props.children)) {
-            return this.renderEmpty();
-        }
-        
-        return this.renderNode();
-    },
+        var open     = this.state.open ? ' open' : '';
+        var occupied = this.calculateOccupiedColumns();
+        var columns  = Nodes.get(this.props.node).columns || 4;
 
-    renderNode: function() {
+        var options = _.map([2,4,6,8,10,12], function(value){
+            var disabled = (value <= occupied) ? 'disabled' : '';
+            var selected = (value == columns) ? <i className="fa fa-check"></i> : '';
+
+            return <li className={disabled} key={value}><a href="#" onClick={this.selectColumnsValue.bind(this, value)}>{value} {selected}</a></li>
+        }.bind(this));
+
         return (
-            <div className="row">
-                {this.props.children}
+            <div className={"btn-group pull-right" + open}>
+                <button type="button" className="btn btn-xs dropdown-toggle" onClick={this.toggleOpen}>
+                    Column Limit <span className="caret"></span>
+                </button>
+                <ul className="dropdown-menu">
+                    {options}
+                </ul>
             </div>
-        );
+        )
     },
 
-    renderEmpty: function() {
+    calculateOccupiedColumns: function() {
+        var children = Nodes.get(this.props.node).children || [];
+        var count    = 0;
+
+        _.each(children, function(node) {
+            count += Nodes.get(node).span || 0;
+        });
+
+        return count;
+    },
+
+    selectColumnsValue: function(value) {
+        // TODO: Execute this through an action
+        Nodes.getStore(this.props.node).set('columns', value);
+    },
+
+    toggleOpen: function(event) {
+        if(!this.state.open) {
+            this.setState({open: true});
+
+            this.bindClick()
+        }else {
+            this.setState({open: false});
+            this.unbindClick()
+        }
+
+        event.stopPropagation();
+    },
+
+    bindClick: function() {
+        jQuery(window).bind('click.ColumnSelect' + this.props.node, this.toggleOpen);
+    },
+
+    unbindClick: function() {
+        jQuery(window).unbind('click.ColumnSelect' + this.props.node);
+    }
+});
+
+module.exports = React.createClass({
+    //mixins: [LifeCycleMixin],
+
+    render: function() {
         return (
-            <div className="row">
-                <h3>Add a Column</h3>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="controls col-lg-12">
+                        <h3 className="title">
+                            Row
+                            <div className="btn-group pull-right">
+                                <button className="btn btn-xs">
+                                    Add Column <i className="fa fa-plus"></i>
+                                </button>
+                                <button className="btn btn-xs">
+                                    <i className="fa fa-pencil"></i>
+                                </button>
+                                <button className="btn btn-xs">
+                                    <i className="fa fa-copy"></i>
+                                </button>
+                                <button className="btn btn-xs">
+                                    <i className="fa fa-trash"></i>
+                                </button>
+                            </div>
+                            <ColumnSelect node={this.props.id} />
+                        </h3>
+                    </div>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
