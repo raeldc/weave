176044b4c99ddf76c2a4984f5da916dd07f83f44
Israel D. Canasa <raeldc@gmail.com>
2015-05-17 15:09:55 +0800

    New demolayout.js

--- code/core/demolayout.js
+++ code/core/demolayout.js
@@ -1,71 +1,381 @@
-module.exports ={
-    'root' : {
-        id       : 'root',
-        component: 'root',
-        children : ['header']
-    },
-    'header' : {
-        id       : 'header',
-        component: 'row',
-        parent   : 'root',
-        columns  : 12,
-        children : ['col1', 'col2', 'col3']
-    },
-    'col1' : {
-        id       : 'col1',
-        component: 'column',
-        parent   : 'header',
-        colspan  : 2,
-        children : ['content1', 'content2']
-    },
-    'col2' : {
-        id       : 'col2',
-        component: 'column',
-        parent   : 'header',
-        colspan  : 2,
-        children : ['content3', 'content4']
-    },
-    'col3' : {
-        id       : 'col3',
-        component: 'column',
-        parent   : 'header',
-        colspan  : 2,
-        children : ['content5', 'content6']
-    },
-    'content1' : {
-        id       : 'content1',
-        component: 'text',
-        parent   : 'col1',
-        text     : 'Content 1'
-    },
-    'content2' : {
-        id       : 'content2',
-        component: 'text',
-        parent   : 'col1',
-        text     : 'Content 2'
-    },
-    'content3' : {
-        id       : 'content3',
-        component: 'text',
-        parent   : 'col2',
-        text     : 'Content 3'
-    },
-    'content4' : {
-        id       : 'content4',
-        component: 'text',
-        parent   : 'col2',
-        text     : 'Content 4'
-    },
-    'content5' : {
-        id       : 'content5',
-        component: 'text',
-        parent   : 'col3',
-        text     : 'Content 5'
-    },
-    'content6' : {
-        id       : 'content6',
-        component: 'text',
-        parent   : 'col3',
-        text     : 'Content 6'
-    }
-};
+module.exports = {
+   "root": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdyagz-row"
+      ],
+      "element": "div",
+      "classes": "container-fluid",
+      "id": "root",
+      "component": "root"
+   },
+   "i9mdyagz-row": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdq6qy-column",
+         "i9mdj7yr-column",
+         "i9mdpa10-column",
+         "i9mdnkvu-column",
+         "i9mdvtdn-column"
+      ],
+      "element": "div",
+      "columns": "4",
+      "classes": [
+         "row"
+      ],
+      "component": "row",
+      "parent": "root",
+      "id": "i9mdyagz-row"
+   },
+   "i9mdq6qy-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdz8wc-title",
+         "i9mdwqj2-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "4",
+         "laptop": "4",
+         "tablet": "4",
+         "phone": "4"
+      },
+      "component": "column",
+      "parent": "i9mdyagz-row",
+      "id": "i9mdq6qy-column"
+   },
+   "i9mdz8wc-title": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "h1",
+      "text": "Title (click here to edit)",
+      "component": "title",
+      "id": "i9mdz8wc-title",
+      "parent": "i9mdq6qy-column"
+   },
+   "i9mdwqj2-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdwqj2-text",
+      "parent": "i9mdq6qy-column"
+   },
+   "i9mdj7yr-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "4",
+         "tablet": "4",
+         "phone": "4"
+      },
+      "component": "column",
+      "parent": "i9mdyagz-row",
+      "id": "i9mdj7yr-column"
+   },
+   "i9mdpa10-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdku6w-row"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "2",
+         "laptop": "4",
+         "tablet": "4",
+         "phone": "4"
+      },
+      "component": "column",
+      "parent": "i9mdyagz-row",
+      "id": "i9mdpa10-column"
+   },
+   "i9mdnkvu-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "4",
+         "tablet": "4",
+         "phone": "4"
+      },
+      "component": "column",
+      "parent": "i9mdyagz-row",
+      "id": "i9mdnkvu-column"
+   },
+   "i9mdku6w-row": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdw8ca-column",
+         "i9mdqkjj-column",
+         "i9mdt6n7-column",
+         "i9mdkme9-column"
+      ],
+      "element": "div",
+      "columns": "2",
+      "classes": [
+         "row"
+      ],
+      "component": "row",
+      "parent": "i9mdpa10-column",
+      "id": "i9mdku6w-row"
+   },
+   "i9mdw8ca-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdrqjk-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "1",
+         "tablet": "1",
+         "phone": "2"
+      },
+      "component": "column",
+      "parent": "i9mdku6w-row",
+      "id": "i9mdw8ca-column"
+   },
+   "i9mdqkjj-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9me522l-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "1",
+         "tablet": "1",
+         "phone": "2"
+      },
+      "component": "column",
+      "parent": "i9mdku6w-row",
+      "id": "i9mdqkjj-column"
+   },
+   "i9mdt6n7-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdo9ad-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "1",
+         "tablet": "1",
+         "phone": "2"
+      },
+      "component": "column",
+      "parent": "i9mdku6w-row",
+      "id": "i9mdt6n7-column"
+   },
+   "i9mdkme9-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdvkuc-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "1",
+         "laptop": "1",
+         "tablet": "1",
+         "phone": "2"
+      },
+      "component": "column",
+      "parent": "i9mdku6w-row",
+      "id": "i9mdkme9-column"
+   },
+   "i9mdvtdn-column": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [
+         "i9mdxhrj-text",
+         "i9mdrmiz-text"
+      ],
+      "element": "div",
+      "colspan": {
+         "desktop": "4",
+         "laptop": "4",
+         "tablet": "4",
+         "phone": "4"
+      },
+      "component": "column",
+      "parent": "i9mdyagz-row",
+      "id": "i9mdvtdn-column"
+   },
+   "i9mdxhrj-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdxhrj-text",
+      "parent": "i9mdvtdn-column"
+   },
+   "i9mdrmiz-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdrmiz-text",
+      "parent": "i9mdvtdn-column"
+   },
+   "i9mdrqjk-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdrqjk-text",
+      "parent": "i9mdw8ca-column"
+   },
+   "i9me522l-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9me522l-text",
+      "parent": "i9mdqkjj-column"
+   },
+   "i9mdo9ad-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdo9ad-text",
+      "parent": "i9mdt6n7-column"
+   },
+   "i9mdvkuc-text": {
+      "css": {
+         "all": {},
+         "desktop": {},
+         "laptop": {},
+         "tablet": {},
+         "phone": {}
+      },
+      "children": [],
+      "element": "p",
+      "text": "Double click here to edit",
+      "component": "text",
+      "id": "i9mdvkuc-text",
+      "parent": "i9mdkme9-column"
+   }
+}
