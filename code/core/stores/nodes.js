cb1503f1367ee0881e6bbe3f32635393cdc03d54
Israel D. Canasa <raeldc@gmail.com>
2015-05-22 01:29:25 +0800

    [FIX] Trigger the CSS store of the Node when updating css

--- code/core/stores/nodes.js
+++ code/core/stores/nodes.js
@@ -315,8 +315,9 @@ export default new Store({}, UINodeActions, {
     },
 
     onUpdateNodeCSS: function(id, device, property, value) {
-        this.getStore(id).getStore('css').getStore(device).set(property, value)
-        this.getStore(id).trigger(property, value)
+        let css = this.getStore(id).getStore('css')
+            css.getStore(device).set(property, value)
+            css.trigger(property, device, value)
 
         // Trigger Change on main Node Store
         this.trigger('onUpdateNodeCSS', id, device, property, value)
