9f2c823aff894c5e341695201c85c735f91b9cf5
Israel D. Canasa <raeldc@gmail.com>
2015-06-08 11:53:23 +0800

    [Styling] Added Font-size

--- code/core/components/node/configuration/typography/index.js
+++ code/core/components/node/configuration/typography/index.js
@@ -16,12 +16,28 @@ export default class Typography extends Component {
                     <ul className="typography">
                         <li className="font">Font <i className="fa fa-chevron-down pull-right" /></li>
                         <li className="formatting">
-                            <a className="btn fa fa-bold active" />
-                            <a className="btn fa fa-italic" />
+                            <span className="font-size">
+                                <input onChange={this.setInputValue} value={this.state.size} type="number" min="0" max="999" name="fontSize" defaultValue="12" />
+                            </span>
+                            <span className="alignment">
+                                <a className="btn fa fa-align-left active" />
+                                <a className="btn fa fa-align-center" />
+                                <a className="btn fa fa-align-justify" />
+                                <a className="btn fa fa-align-right" />
+                            </span>
+                            <span className="styling">
+                                <a className="btn fa fa-bold active" />
+                                <a className="btn fa fa-italic" />
+                                <a className="btn fa fa-underline" />
+                            </span>
                         </li>
                     </ul>
                 </div>
             </div>
         )
+    }
+
+    setInputValue() {
+
     }
 }
