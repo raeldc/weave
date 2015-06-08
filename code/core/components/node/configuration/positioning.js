0db60d46f81302d6aca454e5a6b4e4bf63658e17
Israel D. Canasa <raeldc@gmail.com>
2015-06-08 11:32:57 +0800

    [Styling] Renamed Formatting to Typography, Alignment to Positioning

--- code/core/components/node/configuration/positioning.js
+++ code/core/components/node/configuration/positioning.js
@@ -3,12 +3,12 @@
 import Component from 'core/component.js'
 import Nodes     from 'core/stores/nodes.js'
 
-export default class Alignment extends Component {
+export default class Positioning extends Component {
     initialState(props) {
         return Nodes.get(props.node)
     }
 
     render() {
-        return <div><h5>Alignment</h5></div>
+        return <div><h5>Positioning</h5></div>
     }
 }
