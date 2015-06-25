9738c158bf582ddbe3c7be971dbcbc9e5ef19511
Israel D. Canasa <raeldc@gmail.com>
2015-06-25 15:50:14 +0800

    [Test] Each CSS object is for a single media query
    Multiple media queries are not managed by the CSS object anymore. This allows the object to be used by another parent object that could manage media queries and the corresponding css.

--- code/core/lib/class/css.js
+++ code/core/lib/class/css.js
@@ -3,89 +3,50 @@
 import Style from 'core/lib/class/style.js'
 
 var key = {
-    stylesheets: Symbol('stylesheets'),
-    media      : Symbol('media')
+    stylesheet: Symbol('stylesheet'),
+    media     : Symbol('media'),
+    query     : Symbol('query')
 }
 
 export default class CSS {
-    constructor() {
-        this[key.stylesheets] = new Map()
-        this[key.media]       = new Map()
-
-        this.registerMedia('all', 'all')
+    constructor(media = 'all', query = 'all') {
+        this[key.stylesheet] = new Map()
+        this[key.media]       = media
+        this[key.query]       = query
     }
 
-    registerMedia(alias, query) {
-        if(query !== undefined) {
-            this[key.media].set(alias, query)
-            this[key.stylesheets].set(alias, new Map())
+    addStyle(selector, style) {
+        if(!this[key.stylesheet].has(selector)) {
+            this[key.stylesheet].set(selector, new Style(selector, style))
+        }
+        else this[key.stylesheet].get(selector).replace(style)
 
-            return this
+        return this
+    }
+
+    getStyle(selector) {
+        if(!this[key.stylesheet].has(selector)) {
+            this.addStyle(selector, {})
         }
 
-        throw new Error(`Trying to Register Media: ${alias} without a Query`)
+        return this[key.stylesheet].get(selector)
     }
 
-    addStyle(selector, style, media = 'all') {
-        if(this[key.media].has(media)) {
-            let stylesheet = this[key.stylesheets].get(media)
-
-            if(!stylesheet.has(selector)) {
-                stylesheet.set(selector, new Style(selector, style))
-            }
-            else stylesheet.get(selector).replace(style)
-
-            this[key.stylesheets].set(media, stylesheet)
-
-            return this
-        }
-
-        throw new Error(`Unregistered Media Alias: ${media}`)
+    getAlias() {
+        return this[key.media]
     }
 
-    getStyle(selector, media = 'all') {
-        if(this[key.media].has(media)) {
-            if(!this[key.stylesheets].get(media).has(selector)) {
-                this.addStyle(selector, {}, media)
-            }
-
-            return this[key.stylesheets].get(media).get(selector)
-        }
-
-        throw new Error(`Unregistered Media Alias: ${media}`)
+    getQuery() {
+        return this[key.query]
     }
 
-    getMediaQuery(alias) {
-        if(this[key.media].has(alias)) {
-            return this[key.media].get(alias)
-        }
-
-        throw new Error(`Unknown Media: ${alias}`)
-    }
-
-    getStylesheet(media = all) {
-        if(this[key.stylesheets].has(media)) {
-            return this[key.stylesheets].get(media)
-        }
-
-        throw new Error(`Unknown Media: ${media}`)
-    }
-
-    toString(media) {
+    toString(inline = false) {
         let css = ''
 
-        for(let [media, stylesheet] of this[key.stylesheets]) {
-            let query = this[key.media].get(media)
-
-            css += `@media ${query} {\n`
-
-            for(let [selector, style] of stylesheet) {
-                css += "\n" + style.toString().replace(/^(.*)$/gm, "\t$1") + "\n"
-            }
-
-            css += '\n}\n'
+        for(let [selector, style] of this[key.stylesheet]) {
+            css += "\n" + style.toString() + "\n"
         }
 
-        return css
+        return inline ? `@media ${this[key.query]} {\n${css.replace(/^(.*)$/gm, "\t$1")}\n}\n` : css
     }
 }
