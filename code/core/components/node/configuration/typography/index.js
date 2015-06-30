54db16f3cb043f157edc52f1cd9154f454dcb171
Israel D. Canasa <raeldc@gmail.com>
2015-06-30 12:21:10 +0800

    [Styling] Use the ColorPicker component in Typography

--- code/core/components/node/configuration/typography/index.js
+++ code/core/components/node/configuration/typography/index.js
@@ -1,11 +1,23 @@
 'use strict'
 
-import CSSConfig from 'core/components/node/configuration/cssconfig.js'
-import Alignment from 'core/components/node/configuration/typography/alignment.js'
-import Transform from 'core/components/node/configuration/typography/transform.js'
+import CSSConfig   from 'core/components/node/configuration/cssconfig.js'
+import Alignment   from 'core/components/node/configuration/typography/alignment.js'
+import Transform   from 'core/components/node/configuration/typography/transform.js'
+import ColorPicker from 'core/components/node/configuration/inputs/colorpicker.js'
+
+// Actions
+import {
+    mergeStyle,
+    toggleStyle,
+    getStyle,
+    getCascade
+} from 'core/actions/styling.js'
 
 export default class Typography extends CSSConfig {
     render() {
+        let style   = getStyle(this.props.node, this.props.device),
+            cascade = getCascade(this.props.node, this.props.device)
+
         return (
             <div className="form-inline config config-typography">
                 <h5>Typography</h5>
@@ -18,11 +30,7 @@ export default class Typography extends CSSConfig {
                                     <a className="btn preset-size clearfix">Normal <i className="fa fa-caret-down pull-right" /></a>
                                     <input onChange={this.setInputValue} value={this.state.size} type="number" min="0" max="999" name="fontSize" defaultValue="12" />
                                 </span>
-
-                                <span className="color">
-                                    <input onChange={this.setInputValue} value={this.state.color} type="text" name="color" defaultValue="#000" />
-                                    <a className="btn color-picker fa fa-eyedropper" />
-                                </span>
+                                <ColorPicker {...this.props} property="color" value={style.get('color', '#000')} />
                             </span>
                         </li>
                         <li className="formatting clearfix">
