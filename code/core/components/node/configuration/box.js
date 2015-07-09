ddde3255d748e49dcddc4d500b74caab5b810260
Israel D. Canasa <raeldc@gmail.com>
2015-07-09 15:01:36 +0800

    [Styling] Allow styling of Borders
    Code is quite messy right now, but so far it works.

--- code/core/components/node/configuration/box.js
+++ code/core/components/node/configuration/box.js
@@ -1,7 +1,8 @@
 'use strict'
 
-import CSSConfig from 'core/components/node/configuration/cssconfig.js'
-import DropDown  from 'core/ui/elements/dropdown.js'
+import CSSConfig        from 'core/components/node/configuration/cssconfig.js'
+import DropDown         from 'core/ui/elements/dropdown.js'
+import ReactColorPicker from 'react-color-picker'
 
 // Actions
 import {
@@ -18,18 +19,25 @@ export default class Box extends CSSConfig {
 
     afterUpdate() {
         if(this.refs.subjectInput) {
-            let input = React.findDOMNode(this.refs.subjectInput)
+            let active = false
 
-            if(document.activeElement !== input) {
-                input.select()
+            // Check if one of the refs are on focus
+            _.each(this.refs, input => {
+                if(document.activeElement === React.findDOMNode(input)) {
+                    active = true
+                }
+            })
+
+            // If not on focus, focus on it
+            if(!active) {
+                React.findDOMNode(this.refs.subjectInput).select()
             }
         }
     }
 
     render() {
-        const style       = getStyle(this.props.node, this.props.device)
-        const cascade     = getCascade(this.props.node, this.props.device)
-        const DropDownBox = this.state.open ? this.getDropDown(style) : null
+        const style   = getStyle(this.props.node, this.props.device)
+        const cascade = getCascade(this.props.node, this.props.device)
 
         return (
             <div className="form-inline config config-box">
@@ -97,36 +105,36 @@ export default class Box extends CSSConfig {
                     </ul>
                     <ul className="border">
                         <li className="title">Border</li>
-                        <li className="top">
-                            <a className="clickField">{this.state.borderTop || 0}</a>
+                        <li className="top" ref="borderTop" onClick={() => {this.openDropdown('borderTop')}}>
+                            <a className="clickField">{style.get('borderTop', '0px')}</a>
                         </li>
-                        <li className="right">
-                            <a className="clickField">{this.state.borderRight || 0}</a>
+                        <li className="right" ref="borderRight" onClick={() => {this.openDropdown('borderRight')}}>
+                            <a className="clickField">{style.get('borderRight', '0px')}</a>
                         </li>
-                        <li className="bottom">
-                            <a className="clickField">{this.state.borderBottom || 0}</a>
+                        <li className="bottom" ref="borderBottom" onClick={() => {this.openDropdown('borderBottom')}}>
+                            <a className="clickField">{style.get('borderBottom', '0px')}</a>
                         </li>
-                        <li className="left">
-                            <a className="clickField">{this.state.borderLeft || 0}</a>
+                        <li className="left" ref="borderLeft" onClick={() => {this.openDropdown('borderLeft')}}>
+                            <a className="clickField">{style.get('borderLeft', '0px')}</a>
                         </li>
                     </ul>
                     <ul className="radius">
                         <li className="title">Radius</li>
-                        <li className="top-left">
-                            <a className="clickField">{this.state.radiusTopLeft || 0}</a>
+                        <li className="top-left" ref="borderTopLeftRadius" onClick={() => {this.openDropdown('borderTopLeftRadius')}}>
+                            <a className="clickField">{style.get('borderTopLeftRadius', '0px')}</a>
                         </li>
-                        <li className="top-right">
-                            <a className="clickField">{this.state.radiusTopRight || 0}</a>
+                        <li className="top-right" ref="borderTopRightRadius" onClick={() => {this.openDropdown('borderTopRightRadius')}}>
+                            <a className="clickField">{style.get('borderTopRightRadius', '0px')}</a>
                         </li>
-                        <li className="bottom-right">
-                            <a className="clickField">{this.state.radiusBottomRight || 0}</a>
+                        <li className="bottom-right" ref="borderBottomRightRadius" onClick={() => {this.openDropdown('borderBottomRightRadius')}}>
+                            <a className="clickField">{style.get('borderBottomRightRadius', '0px')}</a>
                         </li>
-                        <li className="bottom-left">
-                            <a className="clickField">{this.state.radiusBottomLeft || 0}</a>
+                        <li className="bottom-left" ref="borderBottomLeftRadius" onClick={() => {this.openDropdown('borderBottomLeftRadius')}}>
+                            <a className="clickField">{style.get('borderBottomLeftRadius', '0px')}</a>
                         </li>
                     </ul>
                 </div>
-                {DropDownBox}
+                {this.getDropDown(style)}
             </div>
         )
     }
@@ -145,7 +153,30 @@ export default class Box extends CSSConfig {
         })
     }
 
+    closeBorderDropDown(event) {
+        let delayer = setInterval(() => {
+            if(document.activeElement !== React.findDOMNode(this.refs.subjectInput) && document.activeElement !== React.findDOMNode(this.refs.borderColor)) {
+                this.closeDropdown()
+                clearInterval(delayer)
+            }
+        }, 0)
+    }
+
     getDropDown(style) {
+        if(this.state.open) {
+            if(this.state.subject.match(/^margin|^padding/g)) {
+                return this.getSpacingDropDown(style)
+            }else if(this.state.subject.match(/^border(.*)Radius/g)) {
+                return this.getBorderRadiusDropDown(style)
+            }else if(this.state.subject.match(/^border/g)) {
+                return this.getBorderDropDown(style)
+            }
+        }
+
+        return null
+    }
+
+    getSpacingDropDown(style) {
         const allSides = this.state.allSides ? ' active' : ''
         const oneSide  = !this.state.allSides ? ' active' : ''
 
@@ -158,13 +189,91 @@ export default class Box extends CSSConfig {
                     ref="subjectInput"
                     className="input input-xs"
                     onBlur={this.closeDropdown}
-                    onChange={(event) => {this.setStyle(this.state.subject, event.target.value)}}
+                    onChange={event => {this.setStyle(this.state.subject, event.target.value)}}
                     onMouseDown={event => { event.stopPropagation() }}
                 />
-                <div className="btn-group select-sides" role="group">
+                <div className="btn-group select-sides">
                     <a className={"btn btn-default btn-xs" + allSides} onClick={event => {this.setStyle('allSides')}}>All Sides</a>
                     <a className={"btn btn-default btn-xs" + oneSide} onClick={event => {this.setStyle(this.state.subject)}}>{_.toWords(this.state.subject)}</a>
                 </div>
+            </DropDown>
+        )
+    }
+
+    getBorderDropDown(style) {
+        const allSides = this.state.allSides ? ' active' : ''
+        const oneSide  = !this.state.allSides ? ' active' : ''
+
+        return (
+            <DropDown subject={this.refs[this.state.subject]} viewportWidth={300} onMouseDown={event => {event.preventDefault()}}>
+                <div className="form-field border-width">
+                    <span className="label">Thickness</span>
+                    <input onChange={event => {
+                        this.setStyle(this.state.subject+'Width', String(event.target.value).replace(/[^0-9]/g, 'x')+'px')
+                    }} className="input-xs" ref="subjectInput" value={String(style.get(this.state.subject+'Width', '0')).replace(/[^0-9]/g, '')} type="text" name={this.state.subject+'Width'} onMouseDown={event => {event.stopPropagation()}} onBlur={this.closeBorderDropDown} />
+                </div>
+                <div className="form-field border-style">
+                    <span className="label">Border Style</span>
+                    <a className={
+                        "btn btn-default btn-xs border-style-solid"
+                        + String(style.compareProperty(this.state.subject+'Style', 'solid') ? ' active' : '')
+                    }
+                    onClick={() => {
+                        this.setStyle(this.state.subject+'Style', 'solid')
+                    }}><i>&nbsp;</i></a>
+                    <a className={
+                        "btn btn-default btn-xs border-style-dotted"
+                        + String(style.compareProperty(this.state.subject+'Style', 'dotted') ? ' active' : '')
+                    }
+                    onClick={() => {
+                        this.setStyle(this.state.subject+'Style', 'dotted')
+                    }}><i>&nbsp;</i></a>
+                    <a className={
+                        "btn btn-default btn-xs border-style-dashed"
+                        + String(style.compareProperty(this.state.subject+'Style', 'dashed') ? ' active' : '')
+                    }
+                    onClick={() => {
+                        this.setStyle(this.state.subject+'Style', 'dashed')
+                    }}><i>&nbsp;</i></a>
+                    <a className={
+                        "btn btn-default btn-xs border-style-none"
+                        + String(style.compareProperty(this.state.subject+'Style', 'none') || !style.hasProperty(this.state.subject+'Style') ? ' active' : '')
+                    }
+                    onClick={() => {
+                        this.setStyle(this.state.subject+'Style', 'none')
+                    }}><i className="fa fa-times"></i></a>
+                </div>
+                <div className="form-field border-color">
+                    <span className="label">Color</span>
+                    <input
+                        onChange={event => {this.setStyle(this.state.subject+'Color', event.target.value)}}
+                        className="input-md"
+                        value={style.get(this.state.subject+'Color')}
+                        type="text" ref="borderColor"
+                        name={this.state.subject+'Color'}
+                        onMouseDown={event => {event.target.select()}}
+                        onBlur={this.closeBorderDropDown}
+                    />
+                </div>
+                <ReactColorPicker
+                        defaultValue={style.get(this.state.subject+'Color')}
+                        saturationWidth={150}
+                        saturationHeight={150}
+                        onChange={color => {this.setStyle(this.state.subject+'Color', color)}}
+                        onDrag={color => {this.setStyle(this.state.subject+'Color', color)}}
+                />
+                <div className="btn-group select-sides">
+                    <a className={"btn btn-default btn-xs" + allSides} onClick={event => {this.setStyle('allSides')}}>All Sides</a>
+                    <a className={"btn btn-default btn-xs" + oneSide} onClick={event => {this.setStyle(this.state.subject)}}>{_.toWords(this.state.subject)}</a>
+                </div>
+            </DropDown>
+        )
+    }
+
+    getBorderRadiusDropDown(style) {
+        return (
+            <DropDown subject={this.refs[this.state.subject]} viewportWidth={300} onMouseDown={event => {event.preventDefault()}}>
+                Border Radius
             </DropDown>
         )
     }
@@ -180,33 +289,47 @@ export default class Box extends CSSConfig {
         if(property === 'allSides' || this.state.allSides) {
             this.state.allSides = true
 
-            switch(this.state.subject) {
-                case 'marginTop':
-                case 'marginRight':
-                case 'marginBottom':
-                case 'marginLeft':
-                    style = {
-                        marginTop   : value,
-                        marginRight : value,
-                        marginBottom: value,
-                        marginLeft  : value,
-                    }
-                break
+            if(this.state.subject.match(/^margin/g)) {
+                style = {
+                    marginTop   : value,
+                    marginRight : value,
+                    marginBottom: value,
+                    marginLeft  : value,
+                }
+            }else if(this.state.subject.match(/^padding/g)) {
+                style = {
+                    paddingTop   : value,
+                    paddingRight : value,
+                    paddingBottom: value,
+                    paddingLeft  : value,
+                }
+            }else if(this.state.subject.match(/^border/g)) {
+                const Style = getStyle(this.props.node, this.props.device)
+                const borderWidth = property === this.state.subject+'Width' ? value : Style.get(this.state.subject+'Width', 0)
+                const borderStyle = property === this.state.subject+'Style' ? value : Style.get(this.state.subject+'Style', 'none')
+                const borderColor = property === this.state.subject+'Color' ? value : Style.get(this.state.subject+'Color', '')
 
-                case 'paddingTop':
-                case 'paddingRight':
-                case 'paddingBottom':
-                case 'paddingLeft':
-                    style = {
-                        paddingTop   : value,
-                        paddingRight : value,
-                        paddingBottom: value,
-                        paddingLeft  : value,
-                    }
-                break
+                style = {
+                    borderTopWidth   : borderWidth,
+                    borderTopStyle   : borderStyle,
+                    borderTopColor   : borderColor,
+                    borderRightWidth : borderWidth,
+                    borderRightStyle : borderStyle,
+                    borderRightColor : borderColor,
+                    borderBottomWidth: borderWidth,
+                    borderBottomStyle: borderStyle,
+                    borderBottomColor: borderColor,
+                    borderLeftWidth  : borderWidth,
+                    borderLeftStyle  : borderStyle,
+                    borderLeftColor  : borderColor,
+                }
             }
         }else style[property] = value
 
+        /**
+         * Yes we set the state directly but we are also sure that
+         *  `mergeStyle` will re-render this component.
+         */
         mergeStyle(this.props.node, style, this.props.device)
     }
 }
