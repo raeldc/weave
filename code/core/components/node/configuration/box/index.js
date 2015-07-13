917115145b8aeb7e3e78cb01432f77f363b54228
Israel D. Canasa <raeldc@gmail.com>
2015-07-13 15:45:28 +0800

    [Styling] Separated the Border Radius Config

--- code/core/components/node/configuration/box/index.js
+++ code/core/components/node/configuration/box/index.js
@@ -5,9 +5,10 @@ import DropDown         from 'core/ui/elements/dropdown.js'
 import ReactColorPicker from 'react-color-picker'
 
 // Components
-import Margin  from 'core/components/node/configuration/box/margin.js'
-import Padding from 'core/components/node/configuration/box/padding.js'
-import Border  from 'core/components/node/configuration/box/border.js'
+import Margin       from 'core/components/node/configuration/box/margin.js'
+import Padding      from 'core/components/node/configuration/box/padding.js'
+import Border       from 'core/components/node/configuration/box/border.js'
+import BorderRadius from 'core/components/node/configuration/box/radius.js'
 
 // Actions
 import {
@@ -18,28 +19,6 @@ import {
 } from 'core/actions/styling.js'
 
 export default class Box extends CSSConfig {
-    initialState() {
-        return {open: false, allSides: false}
-    }
-
-    afterUpdate() {
-        if(this.refs.subjectInput) {
-            let active = false
-
-            // Check if one of the refs are on focus
-            _.each(this.refs, input => {
-                if(document.activeElement === React.findDOMNode(input)) {
-                    active = true
-                }
-            })
-
-            // If not on focus, focus on it
-            if(!active) {
-                React.findDOMNode(this.refs.subjectInput).select()
-            }
-        }
-    }
-
     render() {
         const style   = getStyle(this.props.node, this.props.device)
         const cascade = getCascade(this.props.node, this.props.device)
@@ -97,103 +76,9 @@ export default class Box extends CSSConfig {
                     <Margin {...this.props} />
                     <Padding {...this.props} />
                     <Border {...this.props} />
-                    <ul className="radius">
-                        <li className="title">Radius</li>
-                        <li className="top-left" ref="borderTopLeftRadius" onClick={() => {this.openDropdown('borderTopLeftRadius')}}>
-                            <a className="clickField">{style.get('borderTopLeftRadius', '0px')}</a>
-                        </li>
-                        <li className="top-right" ref="borderTopRightRadius" onClick={() => {this.openDropdown('borderTopRightRadius')}}>
-                            <a className="clickField">{style.get('borderTopRightRadius', '0px')}</a>
-                        </li>
-                        <li className="bottom-right" ref="borderBottomRightRadius" onClick={() => {this.openDropdown('borderBottomRightRadius')}}>
-                            <a className="clickField">{style.get('borderBottomRightRadius', '0px')}</a>
-                        </li>
-                        <li className="bottom-left" ref="borderBottomLeftRadius" onClick={() => {this.openDropdown('borderBottomLeftRadius')}}>
-                            <a className="clickField">{style.get('borderBottomLeftRadius', '0px')}</a>
-                        </li>
-                    </ul>
+                    <BorderRadius {...this.props} />
                 </div>
-                {this.getDropDown(style)}
             </div>
         )
-    }
-
-    openDropdown(subject) {
-        this.setState({
-            open   : true,
-            subject: subject
-        })
-    }
-
-    closeDropDown() {
-        this.setState({
-            open   : false,
-            subject: undefined
-        })
-    }
-
-    getDropDown(style) {
-        if(this.state.open) {
-            if(this.state.subject.match(/^border(.*)Radius/g)) {
-                return this.getBorderRadiusDropDown(style)
-            }
-        }
-
-        return null
-    }
-
-    getBorderRadiusDropDown(style) {
-        const allSides = this.state.allSides ? ' active' : ''
-        const oneSide  = !this.state.allSides ? ' active' : ''
-
-        return (
-            <DropDown subject={this.refs[this.state.subject]} viewportWidth={300} onMouseDown={event => {event.preventDefault()}}>
-                <div className="form-field border-radius">
-                    <span className="label">Radius</span>
-                    <input onChange={event => {
-                        this.setStyle(this.state.subject, String(event.target.value).replace(/[^0-9]/g, 'x')+'px')
-                    }} className="input-xs" ref="subjectInput" value={String(style.get(this.state.subject, '0')).replace(/[^0-9]/g, '')} type="text" name={this.state.subject} onMouseDown={event => {event.stopPropagation()}} onBlur={this.closeDropDown} />
-                </div>
-                <div className="btn-group select-sides">
-                    <a className={"btn btn-default btn-xs" + allSides} onClick={event => {this.setStyle('allSides')}}>All Corners</a>
-                    <a className={"btn btn-default btn-xs" + oneSide} onClick={event => {this.setStyle(this.state.subject)}}>{_.toWords(this.state.subject)}</a>
-                </div>
-            </DropDown>
-        )
-    }
-
-    setStyle(property, value) {
-        const
-            subject = this.state.subject || ''
-        let
-            style = {}
-
-        if(value === undefined) {
-            value = React.findDOMNode(this.refs.subjectInput).value
-            this.state.allSides = false
-        }
-
-        if(subject.match(/^border(.*)Radius/g)) {
-            value = value.replace(/[^0-9]/g, '') + 'px'
-        }
-
-        if(property === 'allSides' || this.state.allSides) {
-            this.state.allSides = true
-
-            if(subject.match(/^border(.*)Radius/g)) {
-                style = {
-                    borderTopRightRadius    : value,
-                    borderTopLeftRadius     : value,
-                    borderBottomRightRadius : value,
-                    borderBottomLeftRadius  : value,
-                }
-            }
-        }else style[property] = value
-
-        /**
-         * Yes we set the state directly but we are also sure that
-         *  `mergeStyle` will re-render this component.
-         */
-        mergeStyle(this.props.node, style, this.props.device)
     }
 }
---
+++ code/core/components/node/configuration/box/radius.js
@@ -0,0 +1,91 @@
+'use strict'
+
+import BoxConfig from 'core/components/node/configuration/box/config.js'
+
+// Actions
+import {
+    mergeStyle,
+    getStyle,
+} from 'core/actions/styling.js'
+
+export default class BorderRadius extends BoxConfig {
+    render() {
+        const style = getStyle(this.props.node, this.props.device)
+
+        return (
+            <span>
+                <ul className="radius">
+                    <li className="title">Radius</li>
+                    <li className="top-left" ref="borderTopLeftRadius" onClick={() => {this.openDropdown('borderTopLeftRadius')}}>
+                        <a className="clickField">{style.get('borderTopLeftRadius', '0px')}</a>
+                    </li>
+                    <li className="top-right" ref="borderTopRightRadius" onClick={() => {this.openDropdown('borderTopRightRadius')}}>
+                        <a className="clickField">{style.get('borderTopRightRadius', '0px')}</a>
+                    </li>
+                    <li className="bottom-right" ref="borderBottomRightRadius" onClick={() => {this.openDropdown('borderBottomRightRadius')}}>
+                        <a className="clickField">{style.get('borderBottomRightRadius', '0px')}</a>
+                    </li>
+                    <li className="bottom-left" ref="borderBottomLeftRadius" onClick={() => {this.openDropdown('borderBottomLeftRadius')}}>
+                        <a className="clickField">{style.get('borderBottomLeftRadius', '0px')}</a>
+                    </li>
+                </ul>
+                {this.renderDropDown()}
+            </span>
+        )
+    }
+
+    renderDropDown() {
+        const
+            style    = getStyle(this.props.node, this.props.device),
+            allSides = this.state.allSides ? ' active' : '',
+            oneSide  = !this.state.allSides ? ' active' : ''
+
+        if(this.state.open) {
+            return (
+                <BoxConfig.DropDown subject={this.refs[this.state.subject]} viewportWidth={300} onMouseDown={event => {event.preventDefault()}}>
+                    <div className="form-field border-radius">
+                        <span className="label">Radius</span>
+                        <input onChange={event => {
+                            this.setStyle(this.state.subject, String(event.target.value).replace(/[^0-9]/g, 'x')+'px')
+                        }} className="input-xs" ref="subjectInput" value={String(style.get(this.state.subject, '0')).replace(/[^0-9]/g, '')} type="text" name={this.state.subject} onMouseDown={event => {event.stopPropagation()}} onBlur={event => this.closeDropDown()} />
+                    </div>
+                    <div className="btn-group select-sides">
+                        <a className={"btn btn-default btn-xs" + allSides} onClick={event => {this.setStyle('allSides')}}>All Corners</a>
+                        <a className={"btn btn-default btn-xs" + oneSide} onClick={event => {this.setStyle(this.state.subject)}}>{_.toWords(this.state.subject)}</a>
+                    </div>
+                </BoxConfig.DropDown>
+            )
+        }
+
+        return null
+    }
+
+    setStyle(property, value) {
+        let style = {}
+
+        if(value === undefined) {
+            value = React.findDOMNode(this.refs.subjectInput).value
+            this.state.allSides = false
+        }
+
+        value = value.replace(/[^0-9]/g, '') + 'px'
+
+        if(property === 'allSides' || this.state.allSides) {
+            style = {
+                borderTopRightRadius    : value,
+                borderTopLeftRadius     : value,
+                borderBottomRightRadius : value,
+                borderBottomLeftRadius  : value,
+            }
+
+            /**
+             * Yes we set the state directly but we are also sure that
+             *  `mergeStyle` will re-render this component.
+             */
+            this.state.allSides = true
+        }
+        else style[property] = value
+
+        mergeStyle(this.props.node, style, this.props.device)
+    }
+}
