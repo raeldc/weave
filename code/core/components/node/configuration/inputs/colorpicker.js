868e826e724c61dc12076033b915705570fb9e8d
Wayne Dela Cruz <ddwwcruz@gmail.com>
2016-06-14 10:49:07 +0800

    Removed distance and added x and y to textshadows

--- code/core/components/node/configuration/inputs/colorpicker.js
+++ code/core/components/node/configuration/inputs/colorpicker.js
@@ -1,17 +1,12 @@
 'use strict'
 
-import Component        from 'core/component.js'
-import TinyColor        from 'tinycolor2'
+import Component from 'core/component.js'
+import TinyColor from 'tinycolor2'
 import ReactColorPicker from 'react-color-picker'
-import DropDown         from 'core/ui/elements/dropdown.js'
+import DropDown from 'core/ui/elements/dropdown.js'
 
 // Actions
-import {
-    mergeStyle,
-    toggleStyle,
-    getStyle,
-    getCascade
-} from 'core/actions/styling.js'
+import {mergeStyle, toggleStyle, getStyle, getCascade} from 'core/actions/styling.js'
 
 export default class ColorPicker extends Component {
     initialState() {
@@ -19,17 +14,45 @@ export default class ColorPicker extends Component {
     }
 
     render() {
-        const DropDownBox = this.state.open ? this.getDropDown(): null
-        const open        = this.state.open ? ' open' : ''
-        const color       = TinyColor(this.props.value)
-        const pickerColor = color.isDark() ? color.lighten(50) : color.darken(50)
+        const DropDownBox = this.state.open
+            ? this.getDropDown()
+            : null
+        const open = this.state.open
+            ? ' open'
+            : ''
+        const color = TinyColor(this.props.value)
+        const pickerColor = color.isDark()
+            ? color.lighten(50)
+            : color.darken(50)
 
         return (
             <span className={this.props.className || 'color'}>
-                <input value={this.props.value} type="text" name="color" placeholder="#000" ref="colorInput" onChange={event => {this.changeColor(event.target.value)}} onFocus={this.open} onBlur={this.close} />
-                <a className={"btn"} style={{backgroundColor: this.props.value, position: 'relative'}} onClick={this.focusOnColorInput} onMouseDown={event => {event.preventDefault()}}>
-                    <i className="fa fa-eyedropper" style={{color: pickerColor}} />
-                    {DropDownBox}
+                <input
+                    value={this.props.value}
+                    type="text"
+                    name="color"
+                    placeholder="#000"
+                    ref="colorInput"
+                    onChange={event => {
+                    this.changeColor(event.target.value)
+                }}
+                    onFocus={this.open}
+                    onBlur={this.close}/>
+                <a
+                    className={"btn"}
+                    style={{
+                    backgroundColor: this.props.value,
+                    position: 'relative'
+                }}
+                    onClick={this.focusOnColorInput}
+                    onMouseDown={event => {
+                    event.preventDefault()
+                }}>
+                    <i
+                        className="fa fa-eyedropper"
+                        style={{
+                        color: pickerColor
+                    }}/> {DropDownBox}
                 </a>
             </span>
         )
@@ -39,12 +62,11 @@ export default class ColorPicker extends Component {
         return (
             <DropDown subject={this.refs.colorInput} viewportWidth={300}>
                 <ReactColorPicker
-                        defaultValue={this.props.value}
-                        saturationWidth={220}
-                        saturationHeight={200}
-                        onChange={this.changeColor}
-                        onDrag={this.changeColor}
-                />
+                    defaultValue={this.props.value}
+                    saturationWidth={220}
+                    saturationHeight={200}
+                    onChange={this.changeColor}
+                    onDrag={this.changeColor}/>
             </DropDown>
         )
     }
@@ -62,14 +84,10 @@ export default class ColorPicker extends Component {
     }
 
     open() {
-        this.setState({
-            open: true
-        })
+        this.setState({open: true})
     }
 
     close() {
-        this.setState({
-            open: false
-        })
+        this.setState({open: false})
     }
 }
--- code/core/components/node/configuration/typography/textshadow.js
+++ code/core/components/node/configuration/typography/textshadow.js
@@ -1,14 +1,20 @@
 'use strict'
 
 import Component from 'core/component.js'
+import ColorPicker from 'core/components/node/configuration/inputs/colorpicker.js'
 
 // Actions
-import {replaceStyle, toggleStyle, getStyle, getCascade} from 'core/actions/styling.js'
+import {replaceStyle, toggleStyle, getStyle, getCascade, mergeStyle} from 'core/actions/styling.js'
 
 export default class TextShadow extends Component {
     //
     initialState() {
-        return {distance: 5, blur: 5}
+        // const style = getStyle(this.props.node, this.props.device)
+        return {x: 0, y: 0, blur: 0, color: '#000'}
+    }
+
+    componentWillMount() {
+        this.reStyle()
     }
 
     render() {
@@ -20,34 +26,77 @@ export default class TextShadow extends Component {
             <div {...props}>
                 <h6>Text Shadow</h6>
                 <div>
+                    Color: {state.color}
                     <input
-                        type='range'
+                        type='color'
                         className='input'
-                        min='0'
-                        max='100'
-                        defaultValue={state.distance}
-                        onChange={this.changeDistance.bind(this)}/>
-                    Distance: {state.distance}px
+                        defaultValue={state.color}
+                        onChange={this.changeColor.bind(this)}/>
                 </div>
                 <div>
+                    X: {state.x}px
+                    <input
+                        type='range'
+                        className='input'
+                        min='-10'
+                        max='10'
+                        defaultValue={state.x}
+                        onInput={this.changeX.bind(this)}/>
+                </div>
+                <div>
+                    Y: {- state.y}px
+                    <input
+                        type='range'
+                        className='input'
+                        min='-10'
+                        max='10'
+                        defaultValue={state.y}
+                        onInput={this.changeY.bind(this)}/>
+                </div>
+                <div>
+                    Blur: {state.blur}px
                     <input
                         type='range'
                         className='input'
                         min='0'
-                        max='100'
+                        max='10'
                         defaultValue={state.blur}
                         onChange={this.changeBlur.bind(this)}/>
-                    Blur: {state.blur}px
                 </div>
             </div>
         )
     }
 
-    changeDistance(evt) {
-        this.setState({distance: evt.target.value})
+    changeX(evt) {
+        var state = this.state
+        state.x = parseInt(evt.target.value)
+        this.reStyle()
+    }
+
+    changeY(evt) {
+        var state = this.state
+        state.y = -parseInt(evt.target.value)
+        this.reStyle()
     }
 
     changeBlur(evt) {
-        this.setState({blur: evt.target.value})
+        var state = this.state
+        state.blur = parseInt(evt.target.value)
+        this.reStyle()
+    }
+
+    changeColor(evt) {
+        var state = this.state
+        state.color = evt.target.value
+        this.reStyle()
+    }
+
+    reStyle() {
+        this.setState({})
+        const state = this.state
+        // console.log('(%02d, %02d)', state.x, state.y)
+        mergeStyle(this.props.node, {
+            textShadow: state.x + 'px ' + state.y + 'px ' + state.blur + 'px ' + state.color
+        }, this.props.device)
     }
 }
