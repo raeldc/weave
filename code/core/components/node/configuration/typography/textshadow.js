23af9c30a837e47a0bc46c6deeab6bb93d0aa0a8
Wayne Dela Cruz <ddwwcruz@gmail.com>
2016-06-15 13:07:20 +0800

    textshadow now working properly
    still need to fix css for the slider so that it's more aligned

--- code/core/components/node/configuration/typography/textshadow.js
+++ code/core/components/node/configuration/typography/textshadow.js
@@ -39,70 +39,113 @@ export default class TextShadow extends Component {
         var textShadowParsed = this.parseStyles(textShadowString
             ? textShadowString
             : '0 0 0 #000000')
-        var sets = null
-
-        if (textShadowString) {
-            // sets = (     <div>         <div className='row'>             <div
-            // className='label'>                 Color             </div>             <div
-            // className='comp'>                 <input                     type='color'
-            //         value={textShadowParsed.color} onChange={e => {   this.changeColor(e,
-            // textShadowParsed)              }}/>             </div>        </div>
-            // <hr/> <div className='row'>             <div className='label-wrapper'>
-            // <div className='form-field-group'>         <span className='label'>
-            //               Horizontal </span>                   <input
-            //     type='number'     className='input input-xs' value={textShadowParsed.x}
-            //   onInput={e => {              this.changeX(e, textShadowParsed)
-            // }}/>           </div>             </div>             <div className='comp'>
-            //        <input                     type='range' min='-25'
-            // max='25' value={textShadowParsed.x} onInput={e => {      this.changeX(e,
-            // textShadowParsed)                 }}/> </div>         </div>         <div
-            // className='row'>             <div className='label-wrapper'>
-            // <div className='form-field-group'>                     <span
-            // className='label'> Vertical                     </span>
-            // <input type='number'                         className='input input-xs'
-            // value={textShadowParsed.x}                         onInput={e => {
-            //   this.changeY(e, textShadowParsed)   }}/>                 </div>
-            // </div>             <div className='comp'>                 <input
-            //      type='range'                 min='-25' max='25'
-            // value={textShadowParsed.y}                     onInput={e => {
-            // this.changeY(e, textShadowParsed)                 }}/> </div>         </div>
-            //       <div className='row'>             <div className='label-wrapper'>
-            //      <div className='form-field-group'>                     <span
-            // className='label'>                         Blur                 </span>
-            //         <input type='number'                         className='input
-            // input-xs'            value={textShadowParsed.blur} onInput={e => {
-            //              this.changeBlur(e, textShadowParsed)        }}/>
-            // </div>             </div>             <div className='comp'>
-            // <input                     type='range'             min='0'
-            //   max='25' value={textShadowParsed.blur}                     onInput={e => {
-            // this.changeBlur(e, textShadowParsed)                 }}/> </div> </div>
-            // </div> )
+        if (!textShadowString) {
+            return (
+                <div {...props}>
+                    <ul>
+                        <li className='title'>
+                            Text Shadow
+                            <input
+                                type='checkbox'
+                                className='checkbox'
+                                checked={false}
+                                onClick={() => {
+                                this.toggleActive(false, textShadowParsed)
+                            }}/>
+                        </li>
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <div {...props}>
+                    <ul>
+                        <li className='title'>
+                            Text Shadow
+                            <input
+                                type='checkbox'
+                                className='checkbox'
+                                checked={true}
+                                onClick={() => {
+                                this.toggleActive(true, textShadowParsed)
+                            }}/>
+                        </li>
+                        <li className='display-inline'>
+                            <div className='form-field-group'>
+                                <span className='label'>
+                                    Horizontal
+                                </span>
+                                <input
+                                    type='number'
+                                    className='input input-xs'
+                                    value={textShadowParsed.x}
+                                    onInput={e => {
+                                    this.changeX(e, textShadowParsed)
+                                }}/>
+                                <input
+                                    type='range'
+                                    className='input'
+                                    min='-25'
+                                    max='25'
+                                    value={textShadowParsed.x}
+                                    onInput={e => {
+                                    this.changeX(e, textShadowParsed)
+                                }}/>
+                            </div>
+                        </li>
+                        <li className='display-inline'>
+                            <div className='form-field-group'>
+                                <span className='label'>
+                                    Vertical
+                                </span>
+                                <input
+                                    type='number'
+                                    className='input input-xs'
+                                    value={textShadowParsed.y}
+                                    onInput={e => {
+                                    this.changeY(e, textShadowParsed)
+                                }}/>
+                                <input
+                                    type='range'
+                                    className='input'
+                                    min='-25'
+                                    max='25'
+                                    value={textShadowParsed.y}
+                                    onInput={e => {
+                                    this.changeY(e, textShadowParsed)
+                                }}/>
+                            </div>
+                        </li>
+                        <li className='display-inline'>
+                            <div className='form-field-group'>
+                                <span className='label'>
+                                    Blur
+                                </span>
+                                <input
+                                    type='number'
+                                    className='input input-xs'
+                                    value={textShadowParsed.blur}
+                                    onInput={e => {
+                                    this.changeBlur(e, textShadowParsed)
+                                }}/>
+                                <input
+                                    type='range'
+                                    className='input'
+                                    min='0'
+                                    max='50'
+                                    value={textShadowParsed.blur}
+                                    onInput={e => {
+                                    this.changeBlur(e, textShadowParsed)
+                                }}/>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            )
         }
-
-        return (
-            <div {...props}>
-                <ul>
-                    <li className='title'>
-                        Text Shadow
-                        <input
-                            type='checkbox'
-                            className='checkbox'
-                            checked={textShadowString
-                            ? true
-                            : false}
-                            onClick={() => {
-                            this.toggleActive(textShadowString
-                                ? true
-                                : false, textShadowParsed)
-                        }}/>
-                    </li>
-                </ul>
-            </div>
-        )
     }
 
     toggleActive(active, data) {
-        // console.log(active, data)
         if (active) {
             mergeStyle(this.props.node, {
                 textShadow: null
@@ -116,34 +159,32 @@ export default class TextShadow extends Component {
 
     changeX(event, orig) {
         var x = parseInt(event.target.value)
-        var str = sprintf('%dpx %dpx %dpx %s', x, orig.y, orig.blur, orig.color)
-        mergeStyle(this.props.node, {
-            textShadow: str
-        }, this.props.device)
+        orig.x = x
+        this.reStyle(orig)
     }
 
     changeY(event, orig) {
         var y = parseInt(event.target.value)
-        var str = sprintf('%dpx %dpx %dpx %s', orig.x, y, orig.blur, orig.color)
-        mergeStyle(this.props.node, {
-            textShadow: str
-        }, this.props.device)
+        orig.y = y
+        this.reStyle(orig)
     }
 
     changeBlur(event, orig) {
         var blur = parseInt(event.target.value)
-        var str = sprintf('%dpx %dpx %dpx %s', orig.x, orig.y, blur, orig.color)
-        mergeStyle(this.props.node, {
-            textShadow: str
-        }, this.props.device)
+        orig.blur = blur
+        this.reStyle(orig)
     }
 
     changeColor(event, orig) {
         var color = event.target.value
-        console.log(color)
-        var str = sprintf('%dpx %dpx %dpx %s', orig.x, orig.y, orig.blur, color)
+        orig.color = color
+        this.reStyle(orig)
+    }
+
+    reStyle(data) {
+        var textShadow = sprintf('%dpx %dpx %dpx %s', data.x, data.y, data.blur, data.color)
         mergeStyle(this.props.node, {
-            textShadow: str
+            textShadow
         }, this.props.device)
     }
 }
