2e1ecd219568ba680daf48821970ff9f47188a2d
Israel D. Canasa <raeldc@gmail.com>
2017-08-25 16:28:43 +0800

    Properly initialize Preview, Layout and Customizer
    However . . .
    * CSS is still whacked.
    * Preview is still not displaying the nodes.

--- code/desktop/js/app.js
+++ code/desktop/js/app.js
@@ -32,21 +32,115 @@ CoreBuilder.Components.register(Title)
 CoreBuilder.Components.register(Image)
 
 CoreBuilder.Desktop = function(config) {
-    /**
-     * Render the Layout
-     */
-    ReactDOM.render(
-        <ThemeBuilderLayout />,
-        document.getElementById('corebuilder-layout')
-    )
+    // Initialize the Preview
+    jQuery('#customize-preview > iframe').ready(function() {
+        var preview = this
 
-    /**
-     * Render the Style Settings on the Sidebar
-     */
-    ReactDOM.render(
-        <Customizer />,
-        document.getElementById('corebuilder-customizer')
-    )
+        // Initialize the Nodes (must be replaced with saved values)
+        CoreBuilder.Nodes.setData({root: {component: 'root', id: 'root'}})
+
+        /**
+         * Create Stylesheets for Device Queries
+         * @see https://css-tricks.com/snippets/css/media-queries-for-standard-devices/
+         */
+        Styling.setDocument(preview)
+                .getStylesheets()
+                .create('all',     'all')
+                .create('desktop', 'screen')
+                .create('laptop',  'only screen and (min-width: 992px) and (max-width: 1199px)')
+                .create('tablet',  'only screen and (min-width: 768px) and (max-width: 991px)')
+                .create('phone',   'only screen and (min-width: 320px) and (max-width: 767px)')
+
+        /**
+         * Trigger windowchange on various events
+         */
+        {
+            let $window = jQuery(window)
+
+            function onWindowEvent(event) {
+                LayoutActions.windowChanged(null, event)
+                event.stopPropagation()
+            }
+
+            $window.scroll(onWindowEvent)
+            $window.resize(onWindowEvent)
+        }
+
+        /**
+         * Trigger framechange on various events
+         */
+        {
+            let $preview = jQuery(preview)
+
+            function onFrameEvent(event) {
+                LayoutActions.frameChanged(null, event);
+                event.stopPropagation()
+            }
+
+            $preview.scroll(onFrameEvent)
+            $preview.resize(onFrameEvent)
+            $preview.mouseup(onFrameEvent)
+        }
+
+        /**
+         * Render the Layout
+         */
+        ReactDOM.render(
+            <ThemeBuilderLayout />,
+            document.getElementById('corebuilder-layout')
+        )
+
+        /**
+         * Render the Style Settings on the Sidebar
+         */
+        ReactDOM.render(
+            <Customizer />,
+            document.getElementById('corebuilder-customizer')
+        )
+
+        /**
+         * Set the device of the preview iFrame when it's changed
+         */
+        {
+            let $preview = jQuery(preview)
+
+            $preview.addClass('desktop');
+            LayoutActions.setDevice.listen(function(device) {
+                $preview.removeClass('desktop laptop tablet phone')
+                $preview.addClass(device)
+            });
+        }
+
+        /**
+         * Set the class of #customize-preview on different screenLayouts
+         */
+        {
+            let $preview = jQuery('#customize-preview')
+
+            $preview.addClass('split')
+
+            LayoutActions.setScreenLayout.listen(function (layout) {
+                $preview.removeClass('split full minimized');
+                $preview.addClass(layout);
+            });
+
+            LayoutActions.startResize.listen(function() {
+                $preview.addClass('disable-events')
+            })
+
+            LayoutActions.stopResize.listen(function() {
+                $preview.removeClass('disable-events')
+            })
+
+            LayoutActions.startDrag.listen(function() {
+                $preview.addClass('disable-events')
+            })
+
+            LayoutActions.stopDrag.listen(function() {
+                $preview.removeClass('disable-events')
+            })
+        }
+    })
 }
 
 export default CoreBuilder
